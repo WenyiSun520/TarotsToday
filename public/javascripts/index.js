911a0328439f03b51ede546f02b8f080350fef05
Gisele Fox <>
2022-11-14 16:58:20 -0800

    minor comment change

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,12 @@
 
 async function loadOneCard() {
-  // pull one random card
 
-  // pull all cards
   let response = await fetch("api/readings" + "?numOfCards=1"); // we will change one to a variable we can change 
   let cardData = await response.json();
 
   let htmlReturn = ``
 
-  // for every card in the responsive add a new card to the screen
+  // for every card in the responsive add a new card to the html
   for (let i = 0; i < cardData.length; i++) {
     htmlReturn += `
     <h2>${cardData.name}</h2>
